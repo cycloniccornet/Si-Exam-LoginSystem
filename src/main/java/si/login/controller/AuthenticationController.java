89eb771e721a1d92b7d4d17e25598f59b0625851
cycloniccornet <55379577+cycloniccornet@users.noreply.github.com>
2021-12-13 13:26:04 +0100

    Moved buisness logic to Service layer

--- src/main/java/si/login/controller/AuthenticationController.java
+++ src/main/java/si/login/controller/AuthenticationController.java
@@ -7,16 +7,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import si.login.model.User;
 
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import si.login.repository.UserRepository;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import si.login.service.KafkaService;
+import si.login.service.LoginService;
 
 import java.net.URI;
 import java.util.List;
@@ -25,52 +18,18 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
-
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    KafkaService kafkaService;
+    LoginService loginService;
 
     @GetMapping("/login")
-    public ResponseEntity<Object> authenticateUser(@RequestBody User requestedUser) throws Exception {
-        try {
-            List<User> allUsers = userRepository.findAll();
-            //checks if fetched users does NOT contains an object with requestedUser.getUsername
-            if (allUsers.stream().noneMatch(object -> object.getUsername().equals(requestedUser.getUsername())))
-                return new ResponseEntity<>("Resource not found", HttpStatus.valueOf(404));
-
-            for (User current : allUsers) {
-                if (current.getUsername().equals(requestedUser.getUsername())) {
-                    if (BCrypt.checkpw(requestedUser.getPassword(), current.getPassword())) {
-                        System.out.println(current.toString());
-                        kafkaService.sendUserLoginTopic(current);
-                        return new ResponseEntity<>("Login validated", HttpStatus.valueOf(200));
-                    }
-                    return new ResponseEntity<>("Cannot access the requested resource", HttpStatus.valueOf(403));
-                }
-            }
-
-            return new ResponseEntity<>("The request is incorrect or corrupt, and the server can't understand it.", HttpStatus.valueOf(400));
-        } catch (Exception exception) {
-            if (exception.toString().contains("could not extract ResultSet")) return new ResponseEntity<>("Internal Server Error: \n" + exception, HttpStatus.valueOf(500));
-            return new ResponseEntity<>("The expectation given in the request's Expect header could not be met" + exception, HttpStatus.valueOf(417));
-        }
+    public Object authenticateUser(@RequestBody User requestedUser) throws Exception {
+            return loginService.valdiateLogin(requestedUser);
     }
 
 
     @PostMapping("/register")
-    public ResponseEntity<Object> createNewUser(@RequestBody User requestedUser) {
-        try {
-            requestedUser.setPassword(BCrypt.hashpw(requestedUser.getPassword(), BCrypt.gensalt(10)));
-            User newUser = userRepository.save(requestedUser);
-            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getUserId()).toUri();
-            kafkaService.sendCreateUserTopic(newUser);
-            return new ResponseEntity<>("The user is created" + location, HttpStatus.valueOf(201));
-        } catch (Exception exception) {
-            if (exception.toString().contains("could not extract ResultSet")) return new ResponseEntity<>("Internal Server Error: \n" + exception, HttpStatus.valueOf(500));
-            return new ResponseEntity<>("The expectation given in the request's Expect header could not be met" + exception, HttpStatus.valueOf(417));
-        }
+    public Object createNewUser(@RequestBody User requestedUser) {
+       return loginService.registerUser(requestedUser);
     }
 }
 
--- src/main/java/si/login/controller/UserController.java
+++ src/main/java/si/login/controller/UserController.java
@@ -1,9 +1,6 @@
 package si.login.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.Link;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
@@ -11,87 +8,36 @@ import org.springframework.web.bind.annotation.*;
 import si.login.model.User;
 import si.login.repository.UserRepository;
 import si.login.service.KafkaService;
+import si.login.service.UserService;
 
 import java.util.List;
 import java.util.Optional;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    KafkaService kafkaService;
+    UserService userService;
 
     @GetMapping("/")
     public ResponseEntity<Object> getAllUsers() {
-        try {
-            List<User> allUsers = userRepository.findAll();
-            if (allUsers.isEmpty()) return new ResponseEntity<>("Resource not found" + allUsers, HttpStatus.valueOf(404));
-            return new ResponseEntity<>(allUsers, HttpStatus.valueOf(200));
-        } catch (Exception exception) {
-            if (exception.toString().contains("could not extract ResultSet")) return new ResponseEntity<>(exception, HttpStatus.valueOf(500));
-            return new ResponseEntity<>(exception, HttpStatus.valueOf(403));
-        }
+        return userService.findAllUsers();
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<Object> getUserById(@PathVariable int userId) {
-        try {
-            Optional<User> fetchedUser = userRepository.findById(userId);
-
-            if (fetchedUser.isEmpty()) return new ResponseEntity<>("Resource not found" + fetchedUser, HttpStatus.valueOf(404));
-
-            EntityModel<User> resource = EntityModel.of(fetchedUser.get());
-            WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).getAllUsers());
-            resource.add(linkTo.withRel("all-users"));
-
-            Link selfLink = linkTo(methodOn(this.getClass()).getUserById(userId)).withSelfRel();
-            resource.add(selfLink);
-
-            return new ResponseEntity<>("Resource successfully fetched" + resource, HttpStatus.valueOf(200));
-
-        } catch (Exception exception) {
-            if (exception.toString().contains("could not extract ResultSet")) return new ResponseEntity<>("Internal Server Error: \n" + exception, HttpStatus.valueOf(500));
-            return new ResponseEntity<>("The expectation given in the request's Expect header could not be met: \n" + exception, HttpStatus.valueOf(417));
-        }
+       return userService.getUserById(userId);
     }
 
     @PutMapping("/updateUserById/{userId}")
     public ResponseEntity<String> updateExistingUser(@PathVariable int userId, @RequestBody User requestedUser) {
-        try {
-            User changedUser = userRepository.findById(userId).orElse(null);
-            if (changedUser == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("HTTP 404: Resource not found");
-
-            changedUser.setUsername(requestedUser.getUsername());
-            changedUser.setPassword(BCrypt.hashpw(requestedUser.getPassword(), BCrypt.gensalt(10)));
-            kafkaService.sendUpdateUserTopic(changedUser);
-            userRepository.save(changedUser);
-            return new ResponseEntity<>("The server successfully processed the request, but is not returning any content", HttpStatus.valueOf(204));
-        } catch (Exception exception) {
-            if (exception.toString().contains("could not extract ResultSet")) return new ResponseEntity<>("Internal Server Error: \n" + exception, HttpStatus.valueOf(500));
-            return new ResponseEntity<>("The expectation given in the request's Expect header could not be met" + exception, HttpStatus.valueOf(417));
-        }
+        return userService.updateExistingUser(userId, requestedUser);
     }
 
 
     @DeleteMapping("/deleteUser/{userId}")
     public ResponseEntity<String> deleteExistingUser(@PathVariable int userId) {
-        try {
-            User fetchedUser = userRepository.findById(userId).orElse(null);
-            if (fetchedUser == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("HTTP 404: Resource not found");
-
-            userRepository.delete(fetchedUser);
-            kafkaService.sendDeleteUserTopic(fetchedUser);
-            return new ResponseEntity<>("Resource successfully deleted", HttpStatus.valueOf(200));
-        } catch (Exception exception) {
-            if (exception.toString().contains("could not extract ResultSet")) return new ResponseEntity<>("Internal Server Error: \n" + exception, HttpStatus.valueOf(500));
-            return new ResponseEntity<>("The expectation given in the request's Expect header could not be met" + exception, HttpStatus.valueOf(417));
-        }
+        return userService.deleteExistingUser(userId);
     }
 }
